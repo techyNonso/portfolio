40a0cf1701e133235bc8fcd1df81998ba2d91f6a
william ikeji <williamikeji@gmail.com>
2021-09-08 21:09:32 +0100

    images of projects added

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -2,6 +2,9 @@ import React from 'react'
 import portfolio1 from "../assets/img/portfolio1.jpg"
 import portfolio2 from "../assets/img/portfolio2.jpg"
 import portfolio3 from "../assets/img/portfolio3.jpg"
+import electron from "../assets/img/electron.png"
+import vemon from "../assets/img/vemon.png"
+import { ExternalLink } from 'react-external-link';
 
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
@@ -20,10 +23,10 @@ function Portfolio() {
                 <div className="portfolio__data">
                   <h3 className="portfolio__title">eCommerce Website</h3>
                   <p className="portfolio__description">Website adaptable to all devices, with payment gate ways integrated.</p>
-                  <AnchorLink  href="#" className="button button--flex button--small portfolio__button">
+                  <ExternalLink  href="#" className="button button--flex button--small portfolio__button">
                     Demo
                     <i className="uil uil-arrow-right"></i>
-                  </AnchorLink >
+                  </ExternalLink >
                 </div>
             </div>
 
@@ -34,10 +37,10 @@ function Portfolio() {
                 <div className="portfolio__data">
                   <h3 className="portfolio__title">Company Website</h3>
                   <p className="portfolio__description">A company website giving a pharmacy an online presence.</p>
-                  <AnchorLink  href="#" className="button button--flex button--small portfolio__button">
+                  <ExternalLink  href="#" className="button button--flex button--small portfolio__button">
                     Demo
                     <i className="uil uil-arrow-right"></i>
-                  </AnchorLink >
+                  </ExternalLink >
                 </div>
             </div>
 
@@ -48,10 +51,10 @@ function Portfolio() {
                 <div className="portfolio__data">
                   <h3 className="portfolio__title">Multi vendor site</h3>
                   <p className="portfolio__description">A multivendor  website built with wordpress.</p>
-                  <AnchorLink  href="#" className="button button--flex button--small portfolio__button">
+                  <ExternalLink  href="#" className="button button--flex button--small portfolio__button">
                     Demo
                     <i className="uil uil-arrow-right"></i>
-                  </AnchorLink >
+                  </ExternalLink >
                 </div>
             </div>
 
@@ -62,24 +65,50 @@ function Portfolio() {
                 <div className="portfolio__data">
                   <h3 className="portfolio__title">RX Manager</h3>
                   <p className="portfolio__description">A pharmacy management system built with HTML, CSS, Javascript and PHP.</p>
-                  <AnchorLink  href="#" className="button button--flex button--small portfolio__button">
+                  <ExternalLink  href="#" className="button button--flex button--small portfolio__button">
                     Demo
                     <i className="uil uil-arrow-right"></i>
-                  </AnchorLink >
+                  </ExternalLink >
                 </div>
             </div>
             
-            {/**Portfolio 1 */}
+            {/**Portfolio 5 */}
             <div className="portfolio__content grid swiper-slide">
                 <img src={portfolio1} className="portfolio__img"/>
 
                 <div className="portfolio__data">
                   <h3 className="portfolio__title">Portfolio Website</h3>
                   <p className="portfolio__description">Website adaptable to all devices, built with react.</p>
-                  <AnchorLink  href="#" className="button button--flex button--small portfolio__button">
+                  <ExternalLink  href="#" className="button button--flex button--small portfolio__button">
                     Demo
                     <i className="uil uil-arrow-right"></i>
-                  </AnchorLink >
+                  </ExternalLink >
+                </div>
+            </div>
+            {/**Portfolio 6 */}
+            <div className="portfolio__content grid swiper-slide">
+                <img src={electron} className="portfolio__img"/>
+
+                <div className="portfolio__data">
+                  <h3 className="portfolio__title"> Business manager</h3>
+                  <p className="portfolio__description">A desktop app for business management built with electron Js, couchDb , bootstrap and integrates with the web app through Api.</p>
+                  <ExternalLink  href="#" className="button button--flex button--small portfolio__button">
+                    Demo
+                    <i className="uil uil-arrow-right"></i>
+                  </ExternalLink >
+                </div>
+            </div>
+            {/**Portfolio 7 */}
+            <div className="portfolio__content grid swiper-slide">
+                <img src={vemon} className="portfolio__img"/>
+
+                <div className="portfolio__data">
+                  <h3 className="portfolio__title"> Business manager</h3>
+                  <p className="portfolio__description">A web app version for the business management built with html,css.bootstrap, react Js,redux, djangorestframework,postgressDb and integrates with the desktop through Api.</p>
+                  <ExternalLink  href="#" className="button button--flex button--small portfolio__button">
+                    Demo
+                    <i className="uil uil-arrow-right"></i>
+                  </ExternalLink >
                 </div>
             </div>
           </div>
